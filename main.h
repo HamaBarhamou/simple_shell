83042c158067c9024790423e853bd2a9ab45d0f3
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-05-14 09:16:34 +0100

    remplace strdup     par _strdup

--- main.h
+++ main.h
@@ -31,5 +31,6 @@ char *_strtok(char *str, const char *delim);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 instruction_l *add_nodeinstruction_end(instruction_l **head, char *str);
 instruction_l **list_instrctions(instruction_l **head, char *str);
+char *_strdup(char *src);
 
 #endif
