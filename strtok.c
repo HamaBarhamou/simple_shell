24f9d1e8059af0acbfe8419aed0afea3deac563d
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-05-19 16:44:53 -0700

    strtok function

--- strtok.c
+++ strtok.c
@@ -2,16 +2,78 @@
 #include <string.h>
 
 /**
+ * cmp_chars - compare chars of strings
+ * @str: input string.
+ * @delim: delimiter.
+ *
+ * Return: 1 if are equals, 0 if not.
+ */
+int cmp_chars(char str[], const char *delim)
+{
+	unsigned int i, j, k;
+
+	for (i = 0, k = 0; str[i]; i++)
+	{
+		for (j = 0; delim[j]; j++)
+		{
+			if (str[i] == delim[j])
+			{
+				k++;
+				break;
+			}
+		}
+	}
+	if (i == k)
+		return (1);
+	return (0);
+}
+
+/**
  * _strtok - a function ...
  * @str: the chaine
  * @delim: the chaine
  *
  * Return: 1 or 0
  */
-
-char	*_strtok(char *str, const char *delim)
+char *_strtok(char str[], const char *delim)
 {
-	/* implement your code, see man strtok*/
+	static char *splitted, *str_end;
+	char *str_start;
+	unsigned int i, bool;
 
-	return (strtok(str, delim));
+	if (str != NULL)
+	{
+		if (cmp_chars(str, delim))
+			return (NULL);
+		splitted = str; /*Store first address*/
+		i = _strlen(str);
+		str_end = &str[i]; /*Store last address*/
+	}
+	str_start = splitted;
+	if (str_start == str_end) /*Reaching the end*/
+		return (NULL);
+
+	for (bool = 0; *splitted; splitted++)
+	{
+		/*Breaking loop finding the next token*/
+		if (splitted != str_start)
+			if (*splitted && *(splitted - 1) == '\0')
+				break;
+		/*Replacing delimiter for null char*/
+		for (i = 0; delim[i]; i++)
+		{
+			if (*splitted == delim[i])
+			{
+				*splitted = '\0';
+				if (splitted == str_start)
+					str_start++;
+				break;
+			}
+		}
+		if (bool == 0 && *splitted) /*Str != Delim*/
+			bool = 1;
+	}
+	if (bool == 0) /*Str == Delim*/
+		return (NULL);
+	return (str_start);
 }
